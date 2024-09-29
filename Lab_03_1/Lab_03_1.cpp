4d62a858c14a5102c1caf705ff509374f4de8d67
mINFLEE <maxkotovych@gmail.com>
2024-09-29 16:30:09 +0300

    first commit

---
+++ Lab_03_1/Lab_03_1.cpp
@@ -0,0 +1,42 @@
+﻿#include <iostream>
+#include <cmath>
+
+using namespace std;
+int main() {
+    double x, y;
+    double A;
+    double B;
+
+    cout << "x = ";
+    cin >> x;
+    A = x * x;
+
+    // Спосіб 1: розгалуження в скороченій формі
+    if (x <= -5)
+        B = exp(x - floor(x));
+    if (x > -5 && x <= 3)
+        B = tan(x + 1) - 3.2 + log10(1 + pow(8, x));
+    if (x > 3)
+        B = sqrt(abs(x));
+
+    y = A + B;
+
+    cout << endl;
+    cout << "1) y = " << y << endl;
+
+    // Спосіб 2: розгалуження в повній формі
+    if (x <= -5) {
+        B = exp(x - floor(x));
+    }
+    else if (x > -5 && x <= 3) {
+        B = tan(x + 1) - 3.2 + log10(1 + pow(8, x));
+    }
+    else {
+        B = sqrt(abs(x));
+    }
+    y = A + B;
+
+    cout << "2) y = " << y << endl;
+    cin.get();
+    return 0;
+}
